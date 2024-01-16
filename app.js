f68425bcecf51339d6bbd1346014fc07ca844b2b
Hana Spoustová <bernreih@gmail.com>
2024-01-16 17:27:03 +0100

    reseni upravene

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-console.log('Funguju!')
+let otocPole = [];
 
-const karticky = document.querySelectorAll('.karticka')
+const otocitKarticku = (event) => {
+  const karticka = event.target;
+  karticka.classList.toggle('otocena');
+  karticka.disabled = true;
+  if (otocPole.length < 2) {
+    otocPole.push(karticka);
+  }
+  if (otocPole.length === 2) {
+    if (
+      otocPole[0].querySelector('.obrazek').src ===
+      otocPole[1].querySelector('.obrazek').src
+    ) {
+      otocPole = [];
+    } else {
+      setTimeout(() => {
+        otocPole[0].classList.add('otocena');
+        otocPole[1].classList.add('otocena');
+        otocPole[0].disabled = false;
+        otocPole[1].disabled = false;
+        otocPole = [];
+      }, 1000);
+    }
+  }
+};
 
-karticky[0].classList.remove('otocena')
+const karticky = document.querySelectorAll('.karticka');
+karticky.forEach((karticka) => {
+  karticka.addEventListener('click', otocitKarticku);
+});
